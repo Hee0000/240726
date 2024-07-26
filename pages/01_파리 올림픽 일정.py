2dba761162596bd9359183639d071a78fc8c3f2d
Hee0000 <mailforpemal@gmail.com>
2024-07-26 16:19:24 +0900

    Update 01_파리 올림픽 일정.py

--- pages/01_파리 올림픽 일정.py
+++ pages/01_파리 올림픽 일정.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 st.title('파리 올림픽 일정표 확인하기')
 
-name = st.text_input('이름을 입력해주세요 : ')
+name = st.text_input('대한민국 국가대표 선수들에게 응원의 한마디! : ')
 
 sports = st.selectbox('확인하고 싶은 파리 올림픽 종목을 선택해주세요 : ', 
                       ['육상', '수영', '테니스', '배드민턴', '농구', '배구', '축구'])
 
 if st.button('종목 정보 보기'):
-    st.write(name + '님! ' + sports + '의 일정은 다음과 같습니다.')
+    st.write(name + sports + '의 일정은 다음과 같습니다.')
 
     # 종목의 일정을 보여줍니다.
     schedule_url = 'https://olympics.com/ko/paris-2024/schedule'
