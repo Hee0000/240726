23a252509aa50c22677fe4dab01864b9c29412d9
Hee0000 <mailforpemal@gmail.com>
2024-07-26 15:35:21 +0900

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title('희원의 웹 서비스 만들기')
 name = st.text_input('이름을 입력해주세요 : ')
-menu = st.selectbox('좋아하는 음식을 선택해주세요 : ', ['샌드위치', '아이스크림'])
+menu = st.selectbox('좋아하는 디저트를 선택해주세요 : ', ['과일', '아이스크림', '마카롱', '소금빵', '커피'])
 if st.button('인생말 생성') :
  st.write(name + '님! 당신이 좋아하는 음식은 '+menu+'이군요? 저도 좋아요')
