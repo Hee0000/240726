341be7d39c9e4ad20b6cf1101d10f1ba113a1d41
Hee0000 <mailforpemal@gmail.com>
2024-07-26 16:20:37 +0900

    Update 01_파리 올림픽 일정.py

--- pages/01_파리 올림픽 일정.py
+++ pages/01_파리 올림픽 일정.py
@@ -14,38 +14,29 @@ if st.button('종목 정보 보기'):
     schedule_url = 'https://olympics.com/ko/paris-2024/schedule'
 
     if sports == '육상':
-        st.image(pictogram_url)
         st.write(f'[육상 경기 일정 보기]({schedule_url})')
         st.write('육상 경기 일정: 2024년 8월 1일 - 2024년 8월 11일')
     elif sports == '수영':
-        st.image(pictogram_url)
         st.write(f'[수영 경기 일정 보기]({schedule_url})')
         st.write('수영 경기 일정: 2024년 7월 27일 - 2024년 8월 4일')
     elif sports == '체조':
-        st.image(pictogram_url)
         st.write(f'[체조 경기 일정 보기]({schedule_url})')
         st.write('체조 경기 일정: 2024년 7월 27일 - 2024년 8월 5일')
     elif sports == '유도':
-        st.image(pictogram_url)
         st.write(f'[유도 경기 일정 보기]({schedule_url})')
         st.write('유도 경기 일정: 2024년 7월 27일 - 2024년 8월 3일')
     elif sports == '테니스':
-        st.image(pictogram_url)
         st.write(f'[테니스 경기 일정 보기]({schedule_url})')
         st.write('테니스 경기 일정: 2024년 7월 27일 - 2024년 8월 4일')
     elif sports == '배드민턴':
-        st.image(pictogram_url)
         st.write(f'[배드민턴 경기 일정 보기]({schedule_url})')
         st.write('배드민턴 경기 일정: 2024년 7월 27일 - 2024년 8월 5일')
     elif sports == '농구':
-        st.image(pictogram_url)
         st.write(f'[농구 경기 일정 보기]({schedule_url})')
         st.write('농구 경기 일정: 2024년 7월 27일 - 2024년 8월 10일')
     elif sports == '배구':
-        st.image(pictogram_url)
         st.write(f'[배구 경기 일정 보기]({schedule_url})')
         st.write('배구 경기 일정: 2024년 7월 27일 - 2024년 8월 11일')
     elif sports == '축구':
-        st.image(pictogram_url)
         st.write(f'[축구 경기 일정 보기]({schedule_url})')
         st.write('축구 경기 일정: 2024년 7월 24일 - 2024년 8월 10일')
